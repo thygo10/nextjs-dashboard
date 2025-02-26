0a2d9303e94a5a109c0acdcbb25f624708b80adb
Thiago Farias <thygo10@icloud.com>
2025-02-25 22:01:44 -0300

    up

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from './lib/data';
+import { fetchRevenue, fetchLatestInvoices } from './lib/data';
 
 export default function Page() {
   return (
